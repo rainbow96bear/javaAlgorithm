d3016fbc73688241dca04ee0c0fd77e8ef11fd00
ksjms <ksjms@DESKTOP-S4J28IB>
2022-10-01 22:59:59 +0900

    git 연습

--- hello/src/gitTest.java
+++ hello/src/gitTest.java
@@ -1,10 +1,9 @@
+import java.util.Scanner;
+
+
 
 public class gitTest {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("git 연습");
-		System.out.println("git에 올려보기");
+		System.out.println("git 연습하기");
 	}
-
 }
